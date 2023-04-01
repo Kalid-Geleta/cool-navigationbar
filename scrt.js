62a0b7c517e1d0bbcbe8b8a0ded890e16deb58fd
kalid geleta <kalidgeleta28@gmail.com>
2023-04-01 15:56:02 +0300

    first commit

---
+++ scrt.js
@@ -0,0 +1,6 @@
+document.querySelector('.hamburger').addEventListener('click',()=>{
+  
+    document.querySelector('.hamburger').classList.toggle('clicked')
+    document.querySelector('.navigation').classList.toggle('clicked')
+
+})
